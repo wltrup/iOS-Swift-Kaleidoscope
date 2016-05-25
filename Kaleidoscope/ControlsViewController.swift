9d080efba6cf18dcbf6e7d6e365cd18924cc1074
Wagner Truppel <wagner@novoda.com>
2016-05-26 00:37:47 +0100

    Refactored ControlsViewController.

--- Kaleidoscope/ControlsViewController.swift
+++ Kaleidoscope/ControlsViewController.swift
@@ -10,91 +10,116 @@ import UIKit
 
 
 @objc
-protocol ControlViewDelegate
+protocol ControlsViewControllerDelegate
 {
-    func controlViewDidChangeNumRegionsTo(numRegions: Int)
-    func controlViewDidChangeNumItemsPerRegionsTo(numItemsPerRegion: Int)
-    func controlViewDidChangeItemSizeTo(itemSize: CGFloat)
-    func controlViewDidChangeShowRegionsTo(showRegions: Bool)
+    func showRegionsDidChangeTo(showRegions: Bool)
+    func numRegionsDidChangeTo(numRegions: Int)
+    func numItemsPerRegionsDidChangeTo(numItemsPerRegion: Int)
+    func itemSizeDidChangeTo(itemSize: CGFloat)
+    func itemElasticityDidChangeTo(itemElasticity: CGFloat)
 }
 
 
 class ControlsViewController: UITableViewController
 {
-    weak var delegate: ControlViewDelegate?
+    weak var delegate: ControlsViewControllerDelegate?
+    var model: Model!
 
-    func updateWithViewModel(viewModel: ViewModel)
+    struct Model
     {
-//        let numRegions = viewModel.numRegions
-//        numRegionsLabel?.text = "\(numRegions)"
-//        numRegionsStepper?.value = Double(numRegions)
-//
-//        let numItemsPerRegion = viewModel.numItemsPerRegion
-//        numItemsLabel?.text = "\(numItemsPerRegion)"
-//        numItemsStepper?.value = Double(numItemsPerRegion)
-//
-//        let itemSize = viewModel.itemSize
-//        itemSizeLabel?.text = "\(Int(itemSize))"
-//        itemSizeSlider?.value = Float(itemSize)
-//
-//        let showRegions = viewModel.showRegions
-//        showRegionsSwitch.on = showRegions
+        let showRegions: Bool
+        let numRegions: Int
+        let numItemsPerRegion: Int
+        let itemSize: CGFloat
+        let itemElasticity: CGFloat
     }
 
-    @IBOutlet private weak var     numRegionsLabel: UILabel!
-    @IBOutlet private weak var       numItemsLabel: UILabel!
-    @IBOutlet private weak var       itemSizeLabel: UILabel!
-    @IBOutlet private weak var itemElasticityLabel: UILabel!
+    override func viewWillAppear(animated: Bool)
+    {
+        super.viewWillAppear(animated)
+        updateUI()
+    }
 
-    @IBOutlet private weak var numRegionsStepper: UIStepper!
-    @IBOutlet private weak var   numItemsStepper: UIStepper!
+    @IBOutlet private weak var numRegionsLabel:          UILabel!
+    @IBOutlet private weak var numItemsPerRegionLabel:   UILabel!
+    @IBOutlet private weak var itemSizeLabel:            UILabel!
+    @IBOutlet private weak var itemElasticityLabel:      UILabel!
+    @IBOutlet private weak var numRegionsStepper:        UIStepper!
+    @IBOutlet private weak var numItemsPerRegionStepper: UIStepper!
+    @IBOutlet private weak var itemSizeSlider:           UISlider!
+    @IBOutlet private weak var itemElasticitySlider:     UISlider!
+    @IBOutlet private weak var showRegionsSwitch:        UISwitch!
+}
 
-    @IBOutlet private weak var       itemSizeSlider: UISlider!
-    @IBOutlet private weak var itemElasticitySlider: UISlider!
 
-    @IBOutlet private weak var showRegionsSwitch: UISwitch!
+extension ControlsViewController
+{
+    private func updateUI()
+    {
+        guard model != nil else { fatalError("ControlsViewController model not set") }
+
+        let showRegions = model.showRegions
+        showRegionsSwitch.on = showRegions
+
+        let numRegions = model.numRegions
+        numRegionsLabel?.text = "\(numRegions)"
+        numRegionsStepper?.value = Double(numRegions)
+
+        let numItemsPerRegion = model.numItemsPerRegion
+        numItemsPerRegionLabel?.text = "\(numItemsPerRegion)"
+        numItemsPerRegionStepper?.value = Double(numItemsPerRegion)
+
+        let itemSize = model.itemSize
+        itemSizeLabel?.text = "\(Int(itemSize))"
+        itemSizeSlider?.value = Float(itemSize)
+
+        let itemElasticity = model.itemElasticity
+        itemElasticityLabel?.text = ""
+        itemElasticitySlider?.value = Float(itemElasticity)
+    }
 
     @IBAction private func stepperValueChanged(sender: UIStepper)
     {
-        createViewModel()
-
         switch sender
         {
         case numRegionsStepper:
             let numRegions = Int(numRegionsStepper.value)
-            delegate?.controlViewDidChangeNumRegionsTo(numRegions)
-        case numItemsStepper:
-            let numItemsPerRegion = Int(numItemsStepper.value)
-            delegate?.controlViewDidChangeNumItemsPerRegionsTo(numItemsPerRegion)
+            delegate?.numRegionsDidChangeTo(numRegions)
+        case numItemsPerRegionStepper:
+            let numItemsPerRegion = Int(numItemsPerRegionStepper.value)
+            delegate?.numItemsPerRegionsDidChangeTo(numItemsPerRegion)
         default:
+            fatalError("Unhandled sender in call to stepperValueChanged(:)")
             break
         }
     }
 
     @IBAction private func sliderValueChanged(sender: UISlider)
     {
-        createViewModel()
-        let itemSize = CGFloat(itemSizeSlider.value)
-        delegate?.controlViewDidChangeItemSizeTo(itemSize)
+        switch sender
+        {
+        case itemSizeSlider:
+            let itemSize = CGFloat(itemSizeSlider.value)
+            delegate?.itemSizeDidChangeTo(itemSize)
+        case itemElasticitySlider:
+            let itemElasticity = CGFloat(itemElasticitySlider.value)
+            delegate?.itemElasticityDidChangeTo(itemElasticity)
+        default:
+            fatalError("Unhandled sender in call to sliderValueChanged(:)")
+            break
+        }
     }
 
-    @IBAction private func switchValueChanged()
+    @IBAction private func switchValueChanged(sender: UISwitch)
     {
-        createViewModel()
-        let showRegions = showRegionsSwitch.on
-        delegate?.controlViewDidChangeShowRegionsTo(showRegions)
-    }
-
-    private func createViewModel()
-    {
-        let numRegions = Int(numRegionsStepper.value)
-        let numItemsPerRegion = Int(numItemsStepper.value)
-        let itemSize = CGFloat(itemSizeSlider.value)
-        let showRegions = showRegionsSwitch.on
-
-        let viewModel = ViewModel(numRegions: numRegions, numItemsPerRegion: numItemsPerRegion,
-                                  itemSize: itemSize, showRegions: showRegions)
-
-        updateWithViewModel(viewModel)
+        switch sender
+        {
+        case showRegionsSwitch:
+            let showRegions = showRegionsSwitch.on
+            delegate?.showRegionsDidChangeTo(showRegions)
+        default:
+            fatalError("Unhandled sender in call to switchValueChanged(:)")
+            break
+        }
     }
 }
