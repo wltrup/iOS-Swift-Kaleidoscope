63968d8ac8be80c279fef820a725ad299a3cbb7b
Wagner Truppel <wagner@novoda.com>
2016-05-26 01:23:45 +0100

    More refactoring in ControlsViewController

--- Kaleidoscope/ControlsViewController.swift
+++ Kaleidoscope/ControlsViewController.swift
@@ -9,6 +9,24 @@
 import UIKit
 
 
+struct ValueInRange<T: Comparable>
+{
+    let min: T
+    let max: T
+    let cur: T
+
+    init(min: T, max: T, cur: T)
+    {
+        guard min <= cur && cur <= max else
+        { fatalError("'(\(min), \(cur), \(max))' do not satisfy \(min) ≤ \(cur) ≤ \(max)") }
+
+        self.min = min
+        self.max = max
+        self.cur = cur
+    }
+}
+
+
 @objc
 protocol ControlsViewControllerDelegate
 {
@@ -23,21 +41,30 @@ protocol ControlsViewControllerDelegate
 class ControlsViewController: UITableViewController
 {
     weak var delegate: ControlsViewControllerDelegate?
-    var model: Model!
+    var viewModel: ViewModel!
 
-    struct Model
+    struct ViewModel
     {
-        let showRegions: Bool
-        let numRegions: Int
-        let numItemsPerRegion: Int
-        let itemSize: CGFloat
-        let itemElasticity: CGFloat
+        let showRegions:       Bool
+        let numRegions:        ValueInRange<Int>
+        let numItemsPerRegion: ValueInRange<Int>
+        let itemSize:          ValueInRange<CGFloat>
+        let itemElasticity:    ValueInRange<CGFloat>
     }
 
     override func viewWillAppear(animated: Bool)
     {
         super.viewWillAppear(animated)
         updateUI()
+    }
+
+    private static var itemElasticityNumberFormatter: NSNumberFormatter
+    {
+        let formatter = NSNumberFormatter()
+        formatter.minimumIntegerDigits = 1
+        formatter.minimumFractionDigits = 2
+        formatter.maximumFractionDigits = 2
+        return formatter
     }
 
     @IBOutlet private weak var numRegionsLabel:          UILabel!
@@ -56,26 +83,35 @@ extension ControlsViewController
 {
     private func updateUI()
     {
-        guard model != nil else { fatalError("ControlsViewController model not set") }
+        guard viewModel != nil else { fatalError("ControlsViewController view model not set") }
 
-        let showRegions = model.showRegions
+        let showRegions = viewModel.showRegions
         showRegionsSwitch.on = showRegions
 
-        let numRegions = model.numRegions
-        numRegionsLabel?.text = "\(numRegions)"
-        numRegionsStepper?.value = Double(numRegions)
+        let numRegions = viewModel.numRegions
+        numRegionsLabel.text = "\(numRegions.cur)"
+        numRegionsStepper.minimumValue = Double(numRegions.min)
+        numRegionsStepper.maximumValue = Double(numRegions.max)
+        numRegionsStepper.value = Double(numRegions.cur)
 
-        let numItemsPerRegion = model.numItemsPerRegion
-        numItemsPerRegionLabel?.text = "\(numItemsPerRegion)"
-        numItemsPerRegionStepper?.value = Double(numItemsPerRegion)
+        let numItemsPerRegion = viewModel.numItemsPerRegion
+        numItemsPerRegionLabel.text = "\(numItemsPerRegion.cur)"
+        numItemsPerRegionStepper.minimumValue = Double(numItemsPerRegion.min)
+        numItemsPerRegionStepper.maximumValue = Double(numItemsPerRegion.max)
+        numItemsPerRegionStepper.value = Double(numItemsPerRegion.cur)
 
-        let itemSize = model.itemSize
-        itemSizeLabel?.text = "\(Int(itemSize))"
-        itemSizeSlider?.value = Float(itemSize)
+        let itemSize = viewModel.itemSize
+        itemSizeLabel.text = "\(Int(itemSize.cur))"
+        itemSizeSlider.minimumValue = Float(itemSize.min)
+        itemSizeSlider.maximumValue = Float(itemSize.max)
+        itemSizeSlider.value = Float(itemSize.cur)
 
-        let itemElasticity = model.itemElasticity
-        itemElasticityLabel?.text = ""
-        itemElasticitySlider?.value = Float(itemElasticity)
+        let itemElasticity = viewModel.itemElasticity
+        itemElasticityLabel.text =
+            ControlsViewController.itemElasticityNumberFormatter.stringFromNumber(itemElasticity.cur)
+        itemElasticitySlider.minimumValue = Float(itemElasticity.min)
+        itemElasticitySlider.maximumValue = Float(itemElasticity.max)
+        itemElasticitySlider.value = Float(itemElasticity.cur)
     }
 
     @IBAction private func stepperValueChanged(sender: UIStepper)
@@ -84,9 +120,11 @@ extension ControlsViewController
         {
         case numRegionsStepper:
             let numRegions = Int(numRegionsStepper.value)
+            numRegionsLabel.text = "\(numRegions)"
             delegate?.numRegionsDidChangeTo(numRegions)
         case numItemsPerRegionStepper:
             let numItemsPerRegion = Int(numItemsPerRegionStepper.value)
+            numItemsPerRegionLabel.text = "\(numItemsPerRegion)"
             delegate?.numItemsPerRegionsDidChangeTo(numItemsPerRegion)
         default:
             fatalError("Unhandled sender in call to stepperValueChanged(:)")
@@ -100,9 +138,12 @@ extension ControlsViewController
         {
         case itemSizeSlider:
             let itemSize = CGFloat(itemSizeSlider.value)
+            itemSizeLabel.text = "\(Int(itemSize))"
             delegate?.itemSizeDidChangeTo(itemSize)
         case itemElasticitySlider:
             let itemElasticity = CGFloat(itemElasticitySlider.value)
+            itemElasticityLabel.text =
+                ControlsViewController.itemElasticityNumberFormatter.stringFromNumber(itemElasticity)
             delegate?.itemElasticityDidChangeTo(itemElasticity)
         default:
             fatalError("Unhandled sender in call to sliderValueChanged(:)")
